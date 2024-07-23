b953446fb6f2f2ff39fb6acb79fbc2c1549c94a6
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-07-23 19:47:31 +0530

    feat: Add tailwind forms plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss"
 import defaultTheme from "tailwindcss/defaultTheme"
+import forms from "@tailwindcss/forms"
 
 const config: Config = {
   content: [
@@ -21,6 +22,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [forms],
 }
 export default config
