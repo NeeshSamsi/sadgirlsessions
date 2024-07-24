7df2b1afef73c383685218326e6d006f5246de62
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-07-24 13:28:47 +0530

    feat: Refactor button to add default button props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,37 +1,33 @@
 import { cn } from "@/lib/utils"
-import Link from "next/link"
+import { ButtonHTMLAttributes } from "react"
 
-export default function Button({
-  text,
-  url,
-  theme,
-  type,
-}: {
-  text: string
-  url: string
-  theme: "brand" | "accent"
-  type: "fill" | "text"
-}) {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  theme: {
+    color: "brand" | "accent"
+    type: "fill" | "text"
+  }
+}
+
+export default function Button({ children, theme, type, ...rest }: Props) {
   return (
-    <Link href={url}>
-      <button
-        className={cn(
-          "group relative w-full rounded border-2 border-white px-8 py-4 text-lg font-semibold uppercase transition-colors duration-100",
-          {
-            "bg-brand hover:text-brand focus-visible:text-brand text-white hover:bg-white focus-visible:bg-white":
-              theme === "brand" && type === "fill",
-            "text-brand hover:bg-brand hover:border-brand focus-visible:bg-brand focus-visible:border-brand bg-black hover:text-white focus-visible:text-white":
-              theme === "brand" && type === "text",
-            "bg-accent hover:text-accent focus-visible:text-accent text-white hover:bg-white focus-visible:bg-white":
-              theme === "accent" && type === "fill",
-            "text-accent hover:bg-accent hover:border-accent focus-visible:bg-accent focus-visible:border-accent bg-black hover:text-white focus-visible:text-white":
-              theme === "accent" && type === "text",
-          },
-        )}
-      >
-        <span>{text}</span>
-        <span className="absolute inset-0 -z-10 size-full translate-x-2 translate-y-2 rounded bg-white group-hover:border-2 group-hover:border-white group-hover:bg-black" />
-      </button>
-    </Link>
+    <button
+      className={cn(
+        "group relative w-full rounded border-2 border-white px-8 py-4 text-lg font-semibold uppercase transition-colors duration-100",
+        {
+          "bg-brand text-white hover:bg-white hover:text-brand focus-visible:bg-white focus-visible:text-brand":
+            theme.color === "brand" && theme.type === "fill",
+          "bg-black text-brand hover:border-brand hover:bg-brand hover:text-white focus-visible:border-brand focus-visible:bg-brand focus-visible:text-white":
+            theme.color === "brand" && theme.type === "text",
+          "bg-accent text-white hover:bg-white hover:text-accent focus-visible:bg-white focus-visible:text-accent":
+            theme.color === "accent" && theme.type === "fill",
+          "bg-black text-accent hover:border-accent hover:bg-accent hover:text-white focus-visible:border-accent focus-visible:bg-accent focus-visible:text-white":
+            theme.color === "accent" && theme.type === "text",
+        },
+      )}
+      {...rest}
+    >
+      <span>{children}</span>
+      <span className="absolute inset-0 -z-10 size-full translate-x-2 translate-y-2 rounded bg-white group-hover:border-2 group-hover:border-white group-hover:bg-black" />
+    </button>
   )
 }
